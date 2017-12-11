528d6862bd8f368ab1e18c575721c7d9ceb03bdf
KaroAntonio <karoantonio@gmail.com>
2017-12-10 17:36:04 -0700

    best nb

--- run_naive_bayes.py
+++ run_naive_bayes.py
@@ -8,8 +8,8 @@ from naive_bayes_utils import *
 
 # a script to consolidate training and prediction of naive bayes
 
-TRAINING_FID = 'data/csv/final_data_december_sample.csv'
-VAR_MAP_FID = 'var_maps/dec_var_map.json'
+TRAINING_FID = 'data/csv/all_data.csv'
+VAR_MAP_FID = 'var_maps/best_nb.json'
 OUT_FID = 'out/naive_bayes_dec.csv'
 
 p = prep_data_run_naive_bayes( TRAINING_FID, OUT_FID, VAR_MAP_FID )
