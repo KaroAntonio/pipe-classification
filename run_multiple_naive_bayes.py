3058efa35e38d9c7afe900a27f6fdd1cc6bb6a81
karo <karoantonio@gmail.com>
2019-01-31 17:25:44 +0100

    form joint model output

--- run_multiple_naive_bayes.py
+++ run_multiple_naive_bayes.py
@@ -9,7 +9,7 @@ from naive_bayes_utils import *
 # a script to consolidate training and prediction of naive bayes
 
 TRAINING_FID = 'data/csv/all_data.csv'
-OUT_FID = 'out/naive_bayes_dec.csv'
+OUT_FID = 'out/naive_bayes_{}_2019.csv'
 MODEL_NAMES = [
 	'performance',
 	'criticality',
@@ -17,7 +17,30 @@ MODEL_NAMES = [
 	'mitigation',
 	]
 
+def output_joint_models( res ):
+  # build fid_map
+  fid_map = {}
+  for model_name in MODEL_NAMES:
+    out_col = '{}_Level_PAN'.format(model_name.upper())  
+    pred_col = '{}_model_pred'.format(model_name)
+    for d in res[model_name]['data']:
+      fid = d['FID']
+      if fid not in fid_map: 
+        fid_map[fid] = {}
+        fid_map[fid]['FID'] = d['FID']
+      # the out cols are referred to as mitigation in all cases, for whatever reason
+      fid_map[fid][out_col] = d['MITIGATION_Level_PAN']
+      fid_map[fid][pred_col] = d['mitigation_model_pred']
+
+  return fid_map
+
+res = {}
 for model_name in MODEL_NAMES:
-	var_map_fid = 'var_maps/{}_var_map.json'.format(model_name)
-	p = prep_data_run_naive_bayes( TRAINING_FID, OUT_FID, var_map_fid )
+    var_map_fid = 'var_maps/{}_var_map.json'.format(model_name)
+    res_out_fid = OUT_FID.format(model_name)
+    res[model_name] = prep_data_run_naive_bayes( TRAINING_FID, res_out_fid, var_map_fid )
+
+fid_map = output_joint_models( res )
+fieldnames = ['FID','PERFORMANCE_Level_PAN','performance_model_pred','CRITICALITY_Level_PAN','criticality_model_pred','condition_model_pred','CONDITION_Level_PAN','MITIGATION_Level_PAN','mitigation_model_pred']
+save_data('out/naive_bayes_all_models.csv',fid_map.values(), fieldnames)
 
